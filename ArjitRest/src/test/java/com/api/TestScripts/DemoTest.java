c6a7d3d0c7792d2ac0053af4408b4dae75016e50
Gaurav Rajendra Gaud <gaurav17.gaud@gmail.com>
2020-01-04 21:27:28 +0530

    My First commit through bash

---
+++ ArjitRest/src/test/java/com/api/TestScripts/DemoTest.java
@@ -0,0 +1,11 @@
+package com.api.TestScripts;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void praticeDemoTest()
+	{
+		System.out.println("pratinnce Demo Test running");
+	}
+}
---
+++ ArjitRest/src/test/java/com/api/TestScripts/SampleTest.java
@@ -0,0 +1,12 @@
+package com.api.TestScripts;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void praticeSampleTest()
+	{
+		System.out.println("pratinnceSampleTest running");
+	}
+}
